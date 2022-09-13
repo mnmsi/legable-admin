a75ea6255fada64b31e4fc14d9c8342645d65309
HmHasan <49553112+HmHasan@users.noreply.github.com>
2022-09-13 15:23:14 +0600

    fix image

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -3,19 +3,45 @@
     <x-breadcrumb title="Dashboard" subtitle="Welcome, IOTA Infotech Limited 👋 "
                   buttonIcon="{{asset('image/dashboard/document.svg')}}" buttonText="Upload Files"/>
     {{--    page content start--}}
-    {{--    qick access--}}
-    <div class="row mt-5">
-        <div class="col-lg-4">
-            <x-card icon="{{asset('image/card/card-icon.svg')}}" title="Document" date="Sep 25, 2022, 13:25 PM"/>
-        </div>
-        <div class="col-lg-4">
-            <x-card icon="{{asset('image/card/card-icon.svg')}}" title="Document" date="Sep 25, 2022, 13:25 PM"/>
-        </div>
-        <div class="col-lg-4">
-            <x-card icon="{{asset('image/card/card-icon.svg')}}" title=" Document" date="Sep 25, 2022, 13:25 PM"/>
-        </div>
+    <div class="row mb-5">
+        <div class="col-lg-9">
+            {{--    qick access--}}
+            <div class="row mt-5">
+                <div class="col-lg-4">
+                    <x-card icon="{{asset('image/card/card-icon.svg')}}" title="Document"
+                            date="Sep 25, 2022, 13:25 PM"/>
+                </div>
+                <div class="col-lg-4">
+                    <x-card icon="{{asset('image/card/card-icon.svg')}}" title="Document"
+                            date="Sep 25, 2022, 13:25 PM"/>
+                </div>
+                <div class="col-lg-4">
+                    <x-card icon="{{asset('image/card/card-icon.svg')}}" title=" Document"
+                            date="Sep 25, 2022, 13:25 PM"/>
+                </div>
 
+            </div>
+            <div class="row mt-4">
+                <div class="col-lg-4">
+                    <x-card icon="{{asset('image/card/card-icon.svg')}}" title="Document"
+                            date="Sep 25, 2022, 13:25 PM"/>
+                </div>
+                <div class="col-lg-4">
+                    <x-card icon="{{asset('image/card/card-icon.svg')}}" title="Document"
+                            date="Sep 25, 2022, 13:25 PM"/>
+                </div>
+                <div class="col-lg-4">
+                    <x-card icon="{{asset('image/card/card-icon.svg')}}" title=" Document"
+                            date="Sep 25, 2022, 13:25 PM"/>
+                </div>
+
+            </div>
+            {{--    qick access--}}
+        </div>
+        <div class="col-lg-3">
+            
+        </div>
     </div>
-    {{--    qick access--}}
+
     {{--    page content end--}}
 @endsection
