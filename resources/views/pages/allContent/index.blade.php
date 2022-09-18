6d2ac5450598bac9b20b7d4b063bf273a9f25b86
Mahmudul Hasan <shohag@iotait.tech>
2022-09-18 19:04:03 +0600

    all content responsive

--- resources/views/pages/allContent/index.blade.php
+++ resources/views/pages/allContent/index.blade.php
@@ -1,30 +1,33 @@
 @extends('layouts.app')
 @section('content')
-@include('includes.pageHeader',['title'=>'All Contents','list'=>['Dashboard','All Contents'],'btn'=>[],'link'=>['url'=>'/drawer/upload','text'=>'Upload Content']])
+    <div class="content-page">
+        @include('includes.pageHeader',['title'=>'All Contents','list'=>['Dashboard','All Contents'],'btn'=>[],'link'=>['url'=>'/drawer/upload','text'=>'Upload Content']])
 
-<div class="block-wrapper block-min-height content-wrappers">
-        <div class="top-block">
-            <h6 class="sub-header6 mb-4">
-                Drawers
-            </h6>
-            <div class="conten-items">
-                <x-drawer title="Most Important" url="{{asset('image/card/card-icon.svg')}}" id="drawerid" />
-                <x-drawer title="Documents" url="{{asset('image/card/card-icon.svg')}}" id="drawerid2" />
-                <x-drawer title="Videos" url="{{asset('image/card/card-icon.svg')}}" id="drawerid3" />
+        <div class="block-wrapper block-min-height content-wrappers">
+            <div class="top-block">
+                <h6 class="sub-header6 mb-4">
+                    Drawers
+                </h6>
+                <div class="conten-items">
+                    <x-drawer title="Most Important" url="{{asset('image/card/card-icon.svg')}}" id="drawerid"/>
+                    <x-drawer title="Documents" url="{{asset('image/card/card-icon.svg')}}" id="drawerid2"/>
+                    <x-drawer title="Videos" url="{{asset('image/card/card-icon.svg')}}" id="drawerid3"/>
+                </div>
             </div>
-        </div>
-        <div class="bottom-block">
-            <h6 class="sub-header6 mb-4 border-bottom pb-3 mt-4">
-                All Contents
-            </h6>
-            <div class="conten-items">
-                <x-content title="Certificate" url="{{asset('image/content/demo1.svg')}}" id="contentid" />
-                <x-content title="Contract Fom" url="{{asset('image/content/demo1.svg')}}" id="contentid2"/>
-                <x-content title="goNDA - Notes" url="{{asset('image/content/demo1.svg')}}" id="contentid3"/>
-                <x-content title="Legable concept" url="{{asset('image/content/demo1.svg')}}" id="contentid4"/>
+            <div class="bottom-block">
+                <h6 class="sub-header6 mb-4 border-bottom pb-3 mt-4">
+                    All Contents
+                </h6>
+                <div class="conten-items">
+                    <x-content title="Certificate" url="{{asset('image/content/demo1.svg')}}" id="contentid"/>
+                    <x-content title="Contract Fom" url="{{asset('image/content/demo1.svg')}}" id="contentid2"/>
+                    <x-content title="goNDA - Notes" url="{{asset('image/content/demo1.svg')}}" id="contentid3"/>
+                    <x-content title="Legable concept" url="{{asset('image/content/demo1.svg')}}" id="contentid4"/>
+                </div>
             </div>
         </div>
     </div>
+
     @include('includes.modal.password',['id'=>'','close_id'=>'','class'=>''])
 @endsection
 
--- resources/views/pages/secretDrawer/index.blade.php
+++ resources/views/pages/secretDrawer/index.blade.php
@@ -1,17 +1,19 @@
 @extends('layouts.app')
 @section('content')
-    <x-breadcrumb title="Drawers" subtitle="Everything is encrypted here!" buttonText="+ Add Drawer " link="/drawer/add"/>
-    <div class="block-wrapper block-min-height wrapper-block">
-        <x-card icon="{{asset('image/card/card-icon.svg')}}" title="Document"
-                date="Sep 25, 2022, 13:25 PM"/>
-        <x-card icon="{{asset('image/card/card-icon.svg')}}" title="Document"
-                date="Sep 25, 2022, 13:25 PM"/>
-        <x-card icon="{{asset('image/card/card-icon.svg')}}" title="Document"
-                date="Sep 25, 2022, 13:25 PM"/>
-        <x-card icon="{{asset('image/card/card-icon.svg')}}" title="Document"
-                date="Sep 25, 2022, 13:25 PM"/>
-        <x-card icon="{{asset('image/card/card-icon.svg')}}" title="Document"
-                date="Sep 25, 2022, 13:25 PM"/>
-        <x-card icon="{{asset('image/card/card-icon.svg')}}" title="Document"
-                date="Sep 25, 2022, 13:25 PM"/>
+    <div class="drawer-page">
+        <x-breadcrumb title="Drawers" subtitle="Everything is encrypted here!" buttonText="+ Add Drawer " link="/drawer/add"/>
+        <div class="block-wrapper block-min-height wrapper-block">
+            <x-card icon="{{asset('image/card/card-icon.svg')}}" title="Document"
+                    date="Sep 25, 2022, 13:25 PM"/>
+            <x-card icon="{{asset('image/card/card-icon.svg')}}" title="Document"
+                    date="Sep 25, 2022, 13:25 PM"/>
+            <x-card icon="{{asset('image/card/card-icon.svg')}}" title="Document"
+                    date="Sep 25, 2022, 13:25 PM"/>
+            <x-card icon="{{asset('image/card/card-icon.svg')}}" title="Document"
+                    date="Sep 25, 2022, 13:25 PM"/>
+            <x-card icon="{{asset('image/card/card-icon.svg')}}" title="Document"
+                    date="Sep 25, 2022, 13:25 PM"/>
+            <x-card icon="{{asset('image/card/card-icon.svg')}}" title="Document"
+                    date="Sep 25, 2022, 13:25 PM"/>
+    </div>
 @endsection
