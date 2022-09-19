44133d8f80ce3d0c1f402707eb7477a576253efa
HmHasan <49553112+HmHasan@users.noreply.github.com>
2022-09-19 20:05:07 +0600

    responsive

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,23 +1,23 @@
-<div class="document-card mb-4" id="{{$id}}">
-    <div class="document-card-content-wrapper">
-        <div class="card-icon-wrapper">
-            <div class="card-icon">
-                <img src="{{$icon}}" alt="icon">
-            </div>
-            <div class="three-dot">
-                <img src="{{asset("image/card/dots.svg")}}" alt="dots">
-            </div>
-        </div>
-        <div class="document-card-footer">
-            <div class="document-card-footer-content-wrapper">
-                <div>
-                    <h2 class="document-footer-title">{{$title}}</h2>
-                    <p class="document-footer-date">{{$date}}</p>
-                </div>
-                <div class="document-lock-icon">
-                    <img src="{{asset('image/card/lock.svg')}}" alt="lock">
-                </div>
-            </div>
-        </div>
+<div class="document-card mb-4" id="{{ $id }}">
+ <div class="document-card-content-wrapper">
+  <div class="card-icon-wrapper">
+   <div class="card-icon">
+    <img src="{{ $icon }}" alt="icon">
+   </div>
+   <div class="three-dot">
+    <img src="{{ asset('image/card/dots.svg') }}" alt="dots">
+   </div>
+  </div>
+  <div class="document-card-footer">
+   <div class="document-card-footer-content-wrapper">
+    <div>
+     <h2 class="document-footer-title">{{ $title }}</h2>
+     <p class="document-footer-date">{{ $date }}</p>
     </div>
+    <div class="document-lock-icon">
+     <img src="{{ asset('image/card/lock.svg') }}" alt="lock">
+    </div>
+   </div>
+  </div>
+ </div>
 </div>
