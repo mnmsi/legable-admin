819a7b0fd25682e6b5a6896bcaf9c8acef4ad1a3
HmHasan <49553112+HmHasan@users.noreply.github.com>
2022-09-21 12:18:34 +0600

    billing info

--- resources/views/pages/billing/index.blade.php
+++ resources/views/pages/billing/index.blade.php
@@ -4,8 +4,10 @@
     <div class="block-min-height block-wrapper">
         {{--  main page content--}}
         {{--        title bar start--}}
-        <div class="my-plan-title-bar">
+        <div class="my-plan-title-bar payment-method-title">
             <p>Payment Methods</p>
+            <div class="add-payment-button"><a href=""><img src="{{asset("image/common/squire-add.svg")}}" alt=""></a>
+            </div>
         </div>
         {{-- title bar end--}}
         {{--   payment getway start  --}}
@@ -66,10 +68,10 @@
         </div>
         {{--   payment getway end  --}}
         {{--        billing history start--}}
-        <div class="my-plan-title-bar mt-5 mb-4">
+        <div class="my-plan-title-bar payment-method-title mt-5 mb-4 ">
             <p>Billing History</p>
         </div>
-        <div class="billing-history">
+        <div class="billing-history d-lg-block d-none ">
             <table class="table w-100 table-borderless">
                 <thead>
                 <tr>
@@ -95,6 +97,48 @@
                 </tbody>
             </table>
         </div>
+        {{--        billing history for mobile responsive start--}}
+        <div class="res-single-table-content">
+            <div class="res-single-table-header">
+                <div class="res-single-date">30 June 2022</div>
+                <div class="res-single-price">$ 30.00</div>
+            </div>
+            <div class="res-table-body">
+                <div class="res-plan-text">Legable Premium Plan</div>
+                <div class="res-plan-download">
+                    <a href="#">Download</a>
+                </div>
+            </div>
+        </div>
+        {{--        billing history for mobile responsive end--}}
+        {{--        billing history for mobile responsive start--}}
+        <div class="res-single-table-content">
+            <div class="res-single-table-header">
+                <div class="res-single-date">30 June 2022</div>
+                <div class="res-single-price">$ 30.00</div>
+            </div>
+            <div class="res-table-body">
+                <div class="res-plan-text">Legable Premium Plan</div>
+                <div class="res-plan-download">
+                    <a href="#">Download</a>
+                </div>
+            </div>
+        </div>
+        {{--        billing history for mobile responsive end--}}
+        {{--        billing history for mobile responsive start--}}
+        <div class="res-single-table-content">
+            <div class="res-single-table-header">
+                <div class="res-single-date">30 June 2022</div>
+                <div class="res-single-price">$ 30.00</div>
+            </div>
+            <div class="res-table-body">
+                <div class="res-plan-text">Legable Premium Plan</div>
+                <div class="res-plan-download">
+                    <a href="#">Download</a>
+                </div>
+            </div>
+        </div>
+        {{--        billing history for mobile responsive end--}}
         {{--        billing history end--}}
         {{--  main page end--}}
     </div>
