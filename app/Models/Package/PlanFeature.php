94267d8e1745e701dad644c3b576bed6e16f1f6d
Md.Saiful <saiful@iotait.tech>
2022-10-17 21:22:47 +0600

    make model migration and seeders

---
+++ app/Models/Package/PlanFeature.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Package;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PlanFeature extends Model
+{
+    use HasFactory;
+}
