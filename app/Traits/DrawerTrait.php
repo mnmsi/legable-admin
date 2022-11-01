d411ffa7e2a61cb9d9dcc9fec9f80be5ea8d551a
Md.Saiful <saiful@iotait.tech>
2022-11-01 17:56:23 +0600

    drawer enter

--- app/Traits/DrawerTrait.php
+++ app/Traits/DrawerTrait.php
@@ -6,11 +6,13 @@ use Illuminate\Support\Facades\Hash;
 
 trait DrawerTrait
 {
+    public static $defaultAttr = ['id', 'name', 'content_type', 'is_password_required', 'is_able_use_master_key'];
+
     public function returnItemView($items)
     {
         return view("components.contents.content", [
-            'drawers'  => manipulate_data($items->drawerItems->where('content_type', 'box'), ['id', 'name', 'is_password_required', 'is_able_use_master_key']),
-            'contents' => manipulate_data($items->drawerItems->where('content_type', 'file'), ['id', 'name', 'is_password_required', 'is_able_use_master_key'])
+            'drawers'  => manipulate_data($items->drawerItems->where('content_type', 'box'), self::$defaultAttr),
+            'contents' => manipulate_data($items->drawerItems->where('content_type', 'file'), self::$defaultAttr)
         ]);
     }
 }
