5b7cf1ff08281b47c13bcf8cfe7b9bb9a5cfaee0
Md.Saiful <saiful@iotait.tech>
2022-12-08 17:09:36 +0600

    information add

--- app/Models/Content/InformationData.php
+++ app/Models/Content/InformationData.php
@@ -3,12 +3,11 @@
 namespace App\Models\Content;
 
 use App\Models\BaseModel;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class InformationData extends BaseModel
 {
-    use HasFactory;
-
     protected $table = 'information_data';
 
     protected $fillable = [
@@ -17,4 +16,12 @@ class InformationData extends BaseModel
         'data_type',
         'data',
     ];
+
+    public function data(): Attribute
+    {
+        return Attribute::make(
+            get: fn($value) => myDecrypt($value),
+            set: fn($value) => myEncrypt($value),
+        );
+    }
 }
