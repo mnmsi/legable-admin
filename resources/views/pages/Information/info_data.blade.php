3f458dfb125d46c1d38144c11fae88c386567481
Md.Saiful <saiful@iotait.tech>
2022-12-12 16:29:08 +0600

    information  data draggable

--- resources/views/pages/Information/info_data.blade.php
+++ resources/views/pages/Information/info_data.blade.php
@@ -7,14 +7,14 @@
              id="informationTypeShowDiv">
             <div class="block-wrapper block-min-height content-wrappers">
                 <div class="top-block">
-                    <div class="conten-items">
-                        @foreach($information as $key => $info)
-                            <div class="all-contents" id="{{encrypt($info->id)}}">
-                                <x-information-data :id="encrypt($info->id)"
+                    <div class="conten-items" id="contentsDivId">
+                        {{--@foreach($information as $key => $info)
+                            <div class="all-contents" id="{{$info->id}}">
+                                <x-information-data :id="$info->id"
                                                     :name="$info->name"
                                                     url="{{asset('image/content/demo1.svg')}}"/>
                             </div>
-                        @endforeach
+                        @endforeach--}}
                     </div>
                 </div>
             </div>
@@ -44,11 +44,40 @@
                 tolerance: "pointer",
                 items: ".all-contents",
                 update: function () {
-                    let order = $("#informationTypeShowDiv").sortable('toArray');
-                    console.log(order)
-                    // local.set('informationIds', order);
+                    local.set('informationDataIds', $("#informationTypeShowDiv").sortable('toArray'));
                 }
             });
+
+            let informationDataHtml = '';
+            let informationDataIds = local.get('informationDataIds');
+            let informationData = <?= json_encode($information); ?>;
+            let filterInformationData = [];
+
+            console.log(informationData)
+
+            if (!informationDataIds) {
+                filterInformationData = informationData;
+            } else {
+                for (let infoId in informationDataIds) {
+                    filterInformationData.push(informationData.find(info => info.id.toString() === informationDataIds[infoId]))
+                }
+
+                filterInformationData = filterInformationData.concat(informationData.filter(function (information) {
+                    return !informationDataIds.includes(information.id.toString());
+                }));
+            }
+
+            for (let info of filterInformationData) {
+                informationDataHtml += '<div class="all-contents" id="' + info.id + '">';
+                informationDataHtml += '<div class="content-block-2 text-decoration-none" id="' + info.id + '" onclick="getInformationData(' + info.id + ')">';
+                informationDataHtml += '<img src="{{asset('image/content/demo1.svg')}}" alt="" class="img-fluid top-icon"/>';
+                informationDataHtml += '<div class="footer-item">';
+                informationDataHtml += '<p>' + info.name + '</p>';
+                informationDataHtml += '<img src="{{asset('image/content/lockIcon.svg')}}" alt="" class="img-fluid"/>';
+                informationDataHtml += '</div></div></div>';
+            }
+
+            $("#contentsDivId").html(informationDataHtml);
         });
 
         function getInformationData(id) {
